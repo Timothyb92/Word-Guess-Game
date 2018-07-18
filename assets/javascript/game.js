d2ed92d0d6648566bdd9377401a026e233a1816b
Timothy Bedsole <timbedsole@gmail.com>
2018-07-18 15:09:25 -0400

    added global functions to render guesses left on the screen, and to check for gameover

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 //Empty array that will house user's guesses
-var userGuesses = [];
+var lettersGuessed = [];
 
 //Array that holds possible words for the game
 var gameWords = ["hadoken", "dhalsim", "chunli", "shoryuken", "guile", "zangief"];
@@ -23,8 +23,20 @@ function separateAnswerByLetters(answer){
     }
 }
 
+//updates guesses
+function renderGame(){
+    document.getElementById("guessesP").innerHTML = guesses;
+}
 
-//Function that puts the randomly selected answer in a series of spans (to be styled). needs to be called by a forEach method
+//gameover function
+function gameOverCheck(){
+    if (guesses === 0){
+        document.querySelector("body").innerHTML = "GAME OVER"
+    }
+}
+
+//Function that puts the randomly selected answer in a series of spans (to be styled). 
+//needs to be called by a forEach method
 function putAnswerCharsToSpan (arr){
     var htmlSpan = document.createElement('SPAN');
     htmlSpan.setAttribute('style', 'border-bottom: 1px solid black; padding-left: 5px; margin-left: 10px;');
@@ -41,9 +53,9 @@ var html =
             "<p>Current word</p>" +
             "<p id='answerArray'></p>" +
             "<p>Number of guesses remaining</p>" +
-            "<p>" + guesses + "</p>" + 
+            "<p id=guessesP>" + guesses + "</p>" + 
             "<p>Letters already guessed</p>" +
-            "<p>" + userGuesses + "</p>";
+            "<p>" + lettersGuessed + "</p>";
 
 
 //Adds the game content to the browser and runs the putAnswerCharsToSpan function
@@ -72,25 +84,31 @@ document.onkeyup = function(event){
         for (var j = 0; j < answerCharacters.length; j++){
             if (answerCharacters[j] == keyPress){
                 console.log(keyPress);
-                userGuesses.push(keyPress);
+                lettersGuessed.push(keyPress);
+                renderGame();
             }
         }
-        // If user's key input isn't a character in the answer, guesses is decremented and the letter they guessed is pushed to the userGuesses array
+        // If user's key input isn't a character in the answer, guesses is decremented and the 
+        //letter they guessed is pushed to the lettersGuessed array
         if (answer.indexOf(keyPress) == -1){
             console.log("not a correct guess. you pressed " + keyPress);
             console.log(guesses + " guesses remaining");
             guesses--;
-            userGuesses.push(keyPress);
+            lettersGuessed.push(keyPress);
+            renderGame();
+            gameOverCheck();
         }
         //Checks to see if the key the user pressed is part of the answer. if it is, it reveals the letter to the user
-        for (var k = 0; k < userGuesses.length; k++){
-            for (var l = 0; l < userGuesses.length; l++){
-                if (answerCharacters[l] == userGuesses[k]){
+        for (var k = 0; k < lettersGuessed.length; k++){
+            for (var l = 0; l < lettersGuessed.length; l++){
+                if (answerCharacters[l] == lettersGuessed[k]){
                     //Some code to reveal the letter to the user
                 }
             }
         }
     }
+
+    //if user input isn't a letter, this alerts them
     else {
         alert("Please choose a letter");
     }
